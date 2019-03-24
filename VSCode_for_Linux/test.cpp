cb6f108c6c1056623d925275e1943b5846bd6400
MRwangmaomao <30722103+MRwangmaomao@users.noreply.github.com>
2019-03-24 20:23:14 +0800

    VSCode for Linux

---
+++ VSCode_for_Linux/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 1;
+    int c;
+
+    c = a + b;
+
+
+    cout << "Hello world6" << c << a << b << endl;
+    return 0;
+}
